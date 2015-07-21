1b222ee1935f47917ad442802fa965216170ac5d
All4thlulz <itachi_uchiha@y7mail.com>
2015-07-21 21:49:37 +1000

    Dynamic Lighting Tweak Test

---
+++ Assets/Scripts/TestMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMove : MonoBehaviour {
+	public float speed = 10.0F;
+	public float rotationSpeed = 100.0F;
+
+	void FixedUpdate() {
+		float UpDown = Input.GetAxis("Vertical") * speed;
+		float LeftRight = Input.GetAxis("Horizontal") * speed;
+		UpDown *= Time.deltaTime;
+		LeftRight *= Time.deltaTime;
+		transform.Translate(LeftRight, UpDown, 0);
+
+	}
+}
