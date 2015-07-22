cc301a101c8a96e8a860cf9f64226242544a4854
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-07-22 19:28:57 +1000

    Ladders and lights.

---
+++ Assets/Scripts/Level/CaveLamps.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaveLamps : MonoBehaviour {
+
+	public float destroyTimer;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, destroyTimer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
