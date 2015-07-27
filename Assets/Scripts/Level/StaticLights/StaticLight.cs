3c8804a624f7c2684251c2f2be9f603a46532b7c
Jhett Black <jhettblack@hotmail.com>
2015-07-28 01:23:56 +1000

    Added Configurable StaticLight Component/script.
    Usage:
    Add the StaticLight script to a GameObject that contains a light (directional, etc), and a ParticleSystem which is played when "destructing".
    After you add the StaticLight script, you will get the following options:
        LightType determines how you want the light to destruct:
            DELAY simply destroys the light after x seconds that the light exists in the world (when it is instantiated).
            TRIGGER immediately destroys the light after it is notified. You can notify it by using SendMessage("Trigger") on the GameObject containing the script, or get the StaticLight component and call it directly.
            TRIGGER_DELAY combines both, starting the destruction timer after you trigger the StaticLight script.
        AliveTime (when used with the DELAY and TRIGGER_DELAY) is the amount of time (in seconds) until the light is destroyed. Note that the time for the attached particle system is not included with this, so to get the full time between the light being on-screen to off-screen, sum both the AliveTime and the particle system's

---
+++ Assets/Scripts/Level/StaticLights/StaticLight.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class StaticLight : MonoBehaviour
+{
+    //Construction
+    public LightType lightType;
+
+    //Destruction
+    [Range(0.01f, 300.0f)]
+    public float aliveTime;
+
+    private bool isTriggered;
+    private bool destroying;
+
+	// Initialization
+	void Awake ()
+    {
+        isTriggered = false;
+        destroying = false;
+        GetComponent<ParticleSystem>().Stop();
+	}
+
+    public void Trigger()
+    {
+        isTriggered = true;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        bool destroy = false;
+
+	    switch(lightType)
+        {
+            case LightType.DELAY:
+            {
+                aliveTime -= Time.deltaTime;
+
+                if(aliveTime <= 0)
+                {
+                    destroy = true;
+                }
+                break;
+            }
+            case LightType.TRIGGER:
+            {
+                if(isTriggered)
+                {
+                    destroy = true;
+                }
+                break;
+            }
+            case LightType.TRIGGER_DELAY:
+            {
+                if (isTriggered)
+                {
+                    aliveTime -= Time.deltaTime;
+
+                    if (aliveTime <= 0)
+                    {
+                        destroy = true;
+                    }
+                }
+                break;
+            }
+        }
+
+        if(destroy && !destroying)
+        {
+            destroying = true;
+
+            //Destroy The Light
+            GetComponent<Light>().enabled = false;
+
+            //Play particle, then destroy this GameObject
+            ParticleSystem dest = GetComponent<ParticleSystem>();
+            dest.Play();
+            Destroy(this.gameObject, dest.duration);
+        }
+	}
+}
