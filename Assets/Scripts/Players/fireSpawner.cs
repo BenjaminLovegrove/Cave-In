18c80eb1f0331a12925401eaec000bd6b283ef28
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-07-29 12:36:42 +1000

    Fixed fire spawning in air.

--- Assets/Scripts/Players/fireSpawner.cs
+++ Assets/Scripts/Players/fireSpawner.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class fireSpawner : MonoBehaviour {
 
 	public int touchingFire = 0;
-	float fireCooldown = 0.5f;
+	float fireCooldown = 0f;
 	float lifetime = 5f;
 	public GameObject fireObj;
 	float lifetimeSet;
@@ -15,10 +15,15 @@ public class fireSpawner : MonoBehaviour {
 		lifetime -= Time.deltaTime;
 
 		lifetimeSet = (lifetime / 5f) * 30f;
-		if ((touchingFire <= 0) && (fireCooldown <= 0)){
-			GameObject fire = Instantiate (fireObj, transform.position, Quaternion.identity) as GameObject;
-			fire.SendMessage ("SetLifetime", lifetimeSet);
-			fireCooldown = 1f;
+		RaycastHit rayHit;
+		if (Physics.Raycast(transform.position, Vector3.down, out rayHit, 0.2f)){
+			if (rayHit.collider.gameObject.tag == "Terrain"){
+				if ((touchingFire <= 0) && (fireCooldown <= 0)){
+					GameObject fire = Instantiate (fireObj, transform.position, Quaternion.identity) as GameObject;
+					fire.SendMessage ("SetLifetime", lifetimeSet);
+					fireCooldown = 1f;
+				}
+			}
 		}
 
 		if (lifetime <= 0){
