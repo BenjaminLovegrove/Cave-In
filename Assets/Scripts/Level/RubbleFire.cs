8ec4e399eb405edf75b6bc0d30341337fe630dfe
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-08-05 11:19:21 +1000

    RubbleFire (no sprite)! Min camdis up. Crate fix. Rumble vol down.

---
+++ Assets/Scripts/Level/RubbleFire.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RubbleFire : MonoBehaviour {
+
+	public float lightTime = 1f;
+	bool lit;
+
+	public Light fireLight;
+	public SpriteRenderer fireSprite;
+	AudioSource fireAudio;
+
+	void Start () {
+		fireAudio = GetComponent<AudioSource> ();
+	}
+
+	void OnTriggerStay (Collider col) {
+		if (col.collider.gameObject.tag == "Fire") {
+			lightTime -= Time.deltaTime;
+		}
+	}
+
+	void Update(){
+
+		if (lightTime < 0 && !lit) {
+			fireLight.enabled = true;
+			fireSprite.enabled = true;
+			fireAudio.enabled = true;
+			lit = true;
+		}
+
+	}
+}
