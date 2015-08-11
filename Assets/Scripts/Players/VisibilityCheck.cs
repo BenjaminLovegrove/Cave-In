eeec769dc17e0d29d592e8075d77e9d8104930b3
All4thlulz <itachi_uchiha@y7mail.com>
2015-08-11 17:59:35 +1000

    shit went wrong hopefully it works now

---
+++ Assets/Scripts/Players/VisibilityCheck.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisibilityCheck : MonoBehaviour {
+
+	// Update is called once per frame
+	void LateUpdate () {
+
+		#region check if a player is out of camera view
+		if (Intro.introTimer <= 0 && renderer.isVisible == false)
+			{
+				if (renderer.gameObject.name == "Hickory_sprite")
+				{
+					print (renderer.gameObject.name + "Out of View 01");
+				}
+				if (renderer.gameObject.name == "Sprite")
+				{
+					print (renderer.gameObject.name + "Out of View 02");
+				}
+			}
+		#endregion
+	}
+}
