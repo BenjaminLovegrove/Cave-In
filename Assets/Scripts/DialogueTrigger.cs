edbea0f64da43f7d5a320154fc0c6ef85952628e
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-08-12 23:07:22 +1000

    Implemented some dialogue in rough locations

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueTrigger : MonoBehaviour {
+
+	public AudioClip dialogue;
+	bool triggered = false;
+
+	// Update is called once per frame
+	void OnTriggerEnter (Collider col) {
+	
+		if (col.gameObject.tag == "Player" && triggered == false) {
+			AudioSource.PlayClipAtPoint (dialogue, transform.position);
+			triggered = true;
+		}
+
+	}
+}
