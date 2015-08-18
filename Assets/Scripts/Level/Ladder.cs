9dfb9dff2d1b28774aee58d334fa336df8242978
Benjamin <Benjamin_JL@live.com>
2015-08-18 16:56:58 +1000

    fixed big ladder and stuff

--- Assets/Scripts/Level/Ladder.cs
+++ Assets/Scripts/Level/Ladder.cs
@@ -5,9 +5,10 @@ public class Ladder : MonoBehaviour {
 
 	public bool ladderFall = false;
 	public GameObject ladderParent;
-	//Collider ladderCol;
+	public Collider ladderCol;
 	Rigidbody ladderRb;
 	bool triggered = false;
+
 
 	//public GameObject p1,p2;
 
@@ -18,14 +19,11 @@ public class Ladder : MonoBehaviour {
 
 	void OnTriggerEnter (Collider col) {
 		if (col.gameObject.tag == "Player" && !triggered) {
-			//Destroy (ladderCol);
 
 			ladderRb.isKinematic = false;
 			ladderRb.useGravity = true;
-		//	p1.SendMessage("Ladder", 0f);
-		//	p2.SendMessage("Ladder", 0f);
+			ladderCol.enabled = false;
 			col.gameObject.SendMessage("Ladder", 0f, SendMessageOptions.DontRequireReceiver);
-			Destroy (this.gameObject, 3f);
 		}
 	}
 }
