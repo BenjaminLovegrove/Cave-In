af8f53d2896bfbb01763a82321e690bf40c0c1a1
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-08-24 13:17:45 +1000

    Created joke box. Needs reverb filter (add @ uni).

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -2,16 +2,25 @@
 using System.Collections;
 
 public class DialogueTrigger : MonoBehaviour {
-
+	
+	AudioSource jokeBox;
 	public AudioClip dialogue;
 	bool triggered = false;
 
-	// Update is called once per frame
+	void Start(){
+		jokeBox = GameObject.FindGameObjectWithTag ("JokeBox").GetComponent<AudioSource>();
+	}
+
 	void OnTriggerEnter (Collider col) {
 	
 		if (col.gameObject.tag == "Player" && triggered == false) {
-			AudioSource.PlayClipAtPoint (dialogue, transform.position);
-			triggered = true;
+			if (!jokeBox.isPlaying){
+				AudioSource.PlayClipAtPoint (dialogue, transform.position);
+				triggered = true;
+				jokeBox.gameObject.SendMessage("ResetTimer");
+			} else {
+				jokeBox.gameObject.SendMessage("QueueDialogue", dialogue);
+			}
 		}
 
 	}
---
+++ Assets/Scripts/JokeBox.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JokeBox : MonoBehaviour {
+
+	public AudioClip[] madJokesYo;
+	public float jokeTimer = 13f;
+	public float counter = 0f;
+	AudioSource jokeBox;
+	AudioClip dialogueQueue;
+	int audioSelect;
+
+	void Start () {
+		jokeBox = GetComponent<AudioSource> ();
+		audioSelect = madJokesYo.Length - 1;
+	}
+
+	void Update () {
+		if (Intro.introTimer < -5f) {
+			counter += Time.deltaTime;
+		}
+
+		if (counter > jokeTimer && !jokeBox.isPlaying) {
+			counter = 0f;
+
+			jokeBox.PlayOneShot(madJokesYo[audioSelect]);
+			audioSelect --;
+			counter = 0f;
+		}
+
+		if (audioSelect < 0) {
+			audioSelect = madJokesYo.Length - 1;
+		}
+
+		if (dialogueQueue != null && !jokeBox.isPlaying) {
+			jokeBox.PlayOneShot(dialogueQueue);
+			dialogueQueue = null;
+			counter = 0f;
+		}
+	}
+
+	void QueueDialogue(AudioClip queue){
+		dialogueQueue = queue;
+	}
+
+	void ResetTimer(){
+		counter = 0f;
+	}
+}
