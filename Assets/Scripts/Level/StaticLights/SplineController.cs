4dbfd35adf5e4eb7b6ff73363e2dd8dc122be28d
Jhett Black <jhettblack@hotmail.com>
2015-08-24 15:28:43 +1000

    Added Spline Prefab.
    Implementing:
    Set Position to 0,0,0 and delete existing lights for cave in.
    Change duration variable to correct value (set in the "Spline" named gameobject)
    Change line renderer to desired material/size.

--- Assets/Scripts/Level/StaticLights/SplineController.cs
+++ Assets/Scripts/Level/StaticLights/SplineController.cs
@@ -7,46 +7,59 @@ using System.Collections.Generic;
 [RequireComponent(typeof(LineRenderer))]
 public class SplineController : MonoBehaviour
 {
-	public GameObject splineObject; //The spline object to move.
+    public GameObject splineObjectInst; //The spline object to move.
+
+    private GameObject splineObject;
 
     public List<GameObject> points = new List<GameObject>();    //The spline points
 
     public int vertexCount = 100;                               //Number of vertices to use for rendering/moving along the spline. (Editor/Play Mode)
 
     public float duration = 10;                                 //Duration for spline movement.
-    public bool showPointsInPlayMode = true;                    //Show points in play mode for debugging.
+
+    private bool triggered = false;
 
     //Members
     private SplineInterpolator m_splineInterpolator;            //The spline interpolator instance.                          //The transforms of our spline points.
 
-	void OnDrawGizmos()
-	{
+    void OnDrawGizmos()
+    {
         //Convert our points into an array of transforms.
-		if (points.Count < 2)
+        if (points.Count < 2)
         {
-			return;
+            return;
         }
 
         //Set up our interpolator instance.
-		SplineInterpolator splineInterpolator = GetComponent(typeof(SplineInterpolator)) as SplineInterpolator;
-		SetupSplineInterpolator(splineInterpolator, points);
-		splineInterpolator.StartInterpolation(null, false);
+        SplineInterpolator splineInterpolator = GetComponent(typeof(SplineInterpolator)) as SplineInterpolator;
+        SetupSplineInterpolator(splineInterpolator, points);
+        splineInterpolator.StartInterpolation(null, false);
 
         //Get our LineRenderer instance, and draw the spline in editor.
         LineRenderer lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.SetVertexCount(vertexCount);
 
-		for (int c = 0; c < vertexCount; c++)
-		{
-			float currTime = c * duration / vertexCount;
-			Vector3 currPos = splineInterpolator.GetHermiteAtTime(currTime);
+        for (int c = 0; c < vertexCount; c++)
+        {
+            float currTime = c * duration / vertexCount;
+            Vector3 currPos = splineInterpolator.GetHermiteAtTime(currTime);
 
             lineRenderer.SetPosition(c, currPos);
-		}
-	}
+        }
+    }
 
-	void Start()
-	{
+    public void Trigger()
+    {
+        if (triggered)
+        {
+            return;
+        }
+
+        triggered = true;
+
+        //Create our GameObject
+        splineObject = (GameObject) Instantiate(splineObjectInst, transform.position, transform.rotation);
+
         if (points.Count < 2)
         {
             return;
@@ -71,73 +84,33 @@ public class SplineController : MonoBehaviour
 
         //Begin movement along the spline.
         FollowSpline();
+    }
 
-        //Debugging: Draw Spline GameObjects in play mode.
-        if (!showPointsInPlayMode)
-        {
-            DisableTransforms();
-        }
-
-        else
-        {
-            EnableTransforms();
-        }
-
-	}
-
-	void SetupSplineInterpolator(SplineInterpolator interp, List<GameObject> gameObjects)
-	{
-		interp.Reset();
-
-		float step = duration / (gameObjects.Count - 1);
-
-		for (int i = 0; i < gameObjects.Count; i++)
-		{
-			Transform transform = gameObjects[i].transform;
-			interp.AddPoint(gameObjects[i], transform.position, transform.rotation, step * i, new Vector2(0, 1));
-		}
-	}
-
-	/// <summary>
-	/// Disables the spline objects.
-	/// </summary>
-	void DisableTransforms()
-	{
-        if (points.Count > 1)
-        {
-            foreach (GameObject point in points)
-            {
-                point.SetActive(false);
-            }
-        }
-	}
-
-    /// <summary>
-    /// Enables the spline objects.
-    /// </summary>
-    void EnableTransforms()
+    void SetupSplineInterpolator(SplineInterpolator interp, List<GameObject> gameObjects)
     {
-        if (points.Count > 0)
+        interp.Reset();
+
+        float step = duration / (gameObjects.Count - 1);
+
+        for (int i = 0; i < gameObjects.Count; i++)
         {
-            foreach (GameObject point in points)
-            {
-                point.SetActive(true);
-            }
+            Transform transform = gameObjects[i].transform;
+            interp.AddPoint(gameObjects[i], transform.position, transform.rotation, step * i, new Vector2(0, 1));
         }
     }
 
-	/// <summary>
-	/// Starts moving this GameObject along the spline.
-	/// </summary>
-	void FollowSpline()
-	{
-		if (points.Count > 0)
-		{
-			SetupSplineInterpolator(this.m_splineInterpolator, points);
-			this.m_splineInterpolator.StartInterpolation(delegate(){
-				Destroy (splineObject);
-			}, true);
-			points[0].gameObject.SendMessage("OnSplinePointReached");
-		}
-	}
+    /// <summary>
+    /// Starts moving this GameObject along the spline.
+    /// </summary>
+    void FollowSpline()
+    {
+        if (points.Count > 0)
+        {
+            SetupSplineInterpolator(this.m_splineInterpolator, points);
+            this.m_splineInterpolator.StartInterpolation(delegate(){
+                Destroy (splineObject);
+            }, true);
+            points[0].gameObject.SendMessage("OnSplinePointReached");
+        }
+    }
 }
